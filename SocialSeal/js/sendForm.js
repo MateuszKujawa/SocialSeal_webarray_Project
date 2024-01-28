37f91cd93ec4d269d5855a8dcdf448323d42c814
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2024-01-28 15:54:49 +0100

    Update sendForm.js

--- SocialSeal/js/sendForm.js
+++ SocialSeal/js/sendForm.js
@@ -1,45 +1,3 @@
-// Przykład pobierania inputów
-// Przykład pobierania selectów
-
-
-
-// const inputFuntion = () => {
-//     const nameInput= document.querySelector('#nameInput');
-//     const emailInput= document.querySelector('#email');
-//     const phoneInput= document.querySelector('#phone');
-//     const inputArray = [];
-
-//     inputArray.push("Name: " + nameInput.value);
-//     inputArray.push("Email Adress: " + emailInput.value);
-//     inputArray.push("Phone Number: " + phoneInput.value);
-    
-//     const JsonArray = JSON.stringify(inputArray);
-//     console.log(JsonArray);
-// }
-
-
-// const acceptBtn = document.querySelector('#openPopup2');
-
-// const selectFunction = () => {
-//     const daySelect = document.getElementById('day');
-//     const timeSelect = document.getElementById('time');
-//     const date = daySelect.value;
-//     const time = timeSelect.value;
-
-//     const datetimeSelectArr = [];
-//     datetimeSelectArr.push("Wybrana data: " + date);
-//     datetimeSelectArr.push("Wybrana godzina: " + time);
-
-//     const JsonArray = JSON.stringify(datetimeSelectArr);
-//     console.log(JsonArray);
-// }
-
-// const functions = () => {
-//     inputFuntion();
-//     selectFunction();
-// }
-
-// acceptBtn.addEventListener('click', functions);
 const submitForm = () => {
   // Form
   const form = document.querySelector("#form");
@@ -47,8 +5,9 @@ const submitForm = () => {
   const secondForm = form.querySelector("#popup");
   const thirdForm = form.querySelector("#popup2");
 
-  // Input and select Elements
-  // First form
+ 
+ 
+  // ==== First form ====
   // Inputs
   const nameInput = firstForm.querySelector("#nameInput").value;
   const emailInput = firstForm.querySelector("#email").value;
@@ -56,21 +15,23 @@ const submitForm = () => {
   // Select
   const subjectSelect = firstForm.querySelector("#subject").value;
 
-  // Second form
+
+
+  // ==== Second form ====
   // Select
   const dateSelect = secondForm.querySelector("#dateSelect").value;
   const timeSelect = secondForm.querySelector("#timeSelect").value;
 
 
 
-
-
   // ==== Third form ====
-
+  // Select
+  const expectationSelect = thirdForm.querySelector('#expectationSelect').value;
+  // TextArea
+  const comments = thirdForm.querySelector('#comments').value;
   // Checkboxes
   const checkboxes = thirdForm.querySelectorAll(".checkbox");
-  // Submit btn
-  const submitBtn = thirdForm.querySelector("#submitBtn");
+
 
   checkboxes.forEach(function (checkbox) {
     checkbox.addEventListener("change", () => {
@@ -92,14 +53,40 @@ const submitForm = () => {
     return selectedCheckboxes;
   };
 
-  //Wysyłanie zaznaczonych checkboxów
-  const sendSelectedCheckboxes = () => {
-    const selectedCheckboxes = updateCheckboxes();
-    console.log("Wysyłanie zaznaczonych checkboxów:", selectedCheckboxes);
-  };
+  
 
-  submitBtn.addEventListener("click", sendSelectedCheckboxes);
+  // Form data
+
+  const formData = new FormData();
+  // First form
+  formData.append('name', nameInput);
+  formData.append('email', emailInput);
+  formData.append('phone', phoneInput);
+  formData.append('subject', subjectSelect);
+  // Second form
+  formData.append('day', dateSelect);
+  formData.append('time', timeSelect);
+  // Third form
+  formData.append('expectation', expectationSelect);
+  const selectedCheckboxes = updateCheckboxes();
+  formData.append('platform', JSON.stringify(selectedCheckboxes));
+  formData.append('comments', comments);
+
+
+  // Za cholere nie wiem jaki link mam użyć tego api..
+
+  fetch('URL', {
+    method: 'POST',
+    body: formData,
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("Odpowiedź z serwera", data);
+  })
+  .catch(error => {
+    console.log("Błąd podczas wysyłania danych:", error);
+  });
 }
 
-
-
+const submitButton = document.querySelector('#submitBtn');
+submitButton.addEventListener('click', submitForm);
